00220ef07b9498ef2429d83290dd3526e02cd40c
llARXANGELll <aramh@inbox.ru>
2020-02-29 17:25:18 +0300

    test1

--- src/Work7/Task1/Main.java
+++ src/Work7/Task1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(3);
         System.out.println();
-        System.out.println();
+        System.out.println(7);
         System.out.println();
     }
 }
