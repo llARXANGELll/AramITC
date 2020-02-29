cabcdf25efac51721a5e6801ceba6b8dda2cce98
llARXANGELll <aramh@inbox.ru>
2020-02-29 17:27:25 +0300

    test1

--- src/Work7/Task1/Main.java
+++ src/Work7/Task1/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(3);
         System.out.println();
-        System.out.println(7);
-        System.out.println();
     }
 }
