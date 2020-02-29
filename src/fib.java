8dd30339a9ac76235161e303b9e06fb166fadb5e
llARXANGELll <aramh@inbox.ru>
2020-02-29 15:55:40 +0300

    Обучение1

---
+++ src/fib.java
@@ -0,0 +1,21 @@
+//import java.util.Scanner;
+//
+//public class fib {
+//    static Scanner sc = new Scanner(System.in);
+//    int x;
+//    int y;
+//    public static void fibo (){
+//       int max = 1;
+//       int min;
+//       int x;
+//        for (int i = 0; i < 1; i++) {
+//            min = 1;
+//            max += min;
+//            x += max + min;
+//            System.out.println(x);
+//        }
+//
+//        }
+//    }
+
+
