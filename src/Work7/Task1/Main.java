54fc0a0b42433f2e2eaf01cf87b23888aff317d5
llARXANGELll <aramh@inbox.ru>
2020-02-29 17:29:09 +0300

    test1

--- src/Work7/Task1/Main.java
+++ src/Work7/Task1/Main.java
@@ -2,7 +2,7 @@ package Work7.Task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(2);
+        System.out.println(3);
         System.out.println();
     }
 }
