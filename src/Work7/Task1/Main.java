1c70c7c0455af6a9b9f02cdeede6ba471f2d964a
llARXANGELll <aramh@inbox.ru>
2020-03-01 17:19:46 +0300

    оригинальная ветка

--- src/Work7/Task1/Main.java
+++ src/Work7/Task1/Main.java
@@ -2,7 +2,7 @@ package Work7.Task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(0);
-        System.out.println();
+        System.out.println("оригинальная ветка");
+
     }
 }
